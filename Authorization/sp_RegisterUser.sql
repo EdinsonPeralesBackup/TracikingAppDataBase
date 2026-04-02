19d796b2599f9237b65b131cf4e8487624d05230
Alfiery <alfie@ALF1ERY>
2026-04-02 10:45:04 -0500

    Envio de mensajes para register

--- Authorization/sp_RegisterUser.sql
+++ Authorization/sp_RegisterUser.sql
@@ -13,6 +13,7 @@ CREATE PROCEDURE sp_RegisterUser
 	@pbirthday DATE,
 	@ppassword VARCHAR(MAX),
 	@pphonenumber VARCHAR(20),
+	@presetCode VARCHAR(6),
 	@idUser INT OUTPUT,
 	@message VARCHAR(500) OUTPUT
 )
@@ -31,16 +32,29 @@ BEGIN
 				END
 			ELSE
 				BEGIN
-					DECLARE @idNewUser INT = 0
-					INSERT INTO [USER](Phone, Password, IdRole, State) 
-						VALUES(@pphonenumber, @ppassword, 2, 1)
+					DECLARE @idNewUser INT = 0, @idCode INT
 
-					SET @idNewUser = SCOPE_IDENTITY();
+					SELECT 
+						@idCode = CODE.Id
+					FROM CODE_RESET CODE
+					WHERE Code = @presetCode AND CODE.Phone = @pphonenumber AND CODE.[State] = 1 AND IdUser IS NULL
+					IF(@idCode <> 0)
+					BEGIN
+						INSERT INTO [USER](Phone, Password, IdRole, State) 
+							VALUES(@pphonenumber, @ppassword, 2, 1)
+
+						SET @idNewUser = SCOPE_IDENTITY();
 					
-					INSERT INTO [USER_PROFILE](Name, LastName, Birthday, State, IdUser) VALUES(@pname, @plastName, @pbirthday, 1, @idNewUser)
-
-					SET @idUser = @idNewUser;
-					SET @message = 'User registered successfully.';
+						INSERT INTO [USER_PROFILE](Name, LastName, Birthday, State, IdUser) VALUES(@pname, @plastName, @pbirthday, 1, @idNewUser)
+						UPDATE CODE_RESET SET [State] = 0 WHERE IdUser = @idUser OR Phone = @pphonenumber
+						SET @idUser = @idNewUser;
+						SET @message = 'User registered successfully.';
+					END
+					ELSE
+					BEGIN
+						SET @idUser = 0;
+						SET @message = 'An error occurred during user registration.';
+					END
 				END
 		COMMIT TRANSACTION;
 	END TRY
